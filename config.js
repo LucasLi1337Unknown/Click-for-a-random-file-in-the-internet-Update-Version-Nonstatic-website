d40f057cce3f61834f3b0783f77dacc96aa5f7c4
Lucas Li <bojun792@gmail.com>
2026-09-11 19:57:07 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // REQUIRED for reliable downloads on macOS Chrome/Safari.
 // Deploy /worker with Cloudflare Workers, then paste the workers.dev URL here.
 window.RANDOM_FILE_CONFIG = {
-  workerBaseUrl: ""
+  workerBaseUrl: "https://aged-feather-a710.lucasli37.workers.dev/"
 };
